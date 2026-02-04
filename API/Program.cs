28ae85973f70a8201db3124f7318f5dd23fa2635
Reda <97jertov97@gmail.com>
2026-02-05 01:09:59 +0300

    add .env db connection

--- API/Program.cs
+++ API/Program.cs
@@ -1,14 +1,17 @@
 using API.Data;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Env.Load();
+var dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "db.sqlite";
+
 builder.Services.AddDbContext<AppDbContext>(opt =>
-    opt.UseSqlite("Data Source=db.sqlite"));
+    opt.UseSqlite($"Data Source={dbName}"));
 
 builder.Services.AddScoped<IRollItemService, RollItemService>();
-
 builder.Services.AddControllers();
 
 var app = builder.Build();
